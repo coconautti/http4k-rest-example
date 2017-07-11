881b9fd943e2987a1afc669bdbe60d97fb0a7ead
Peter Hägg <peter@coconautti.com>
2017-07-11 10:02:47 +0300

    Use logging for printouts

--- src/main/kotlin/example/Application.kt
+++ src/main/kotlin/example/Application.kt
@@ -6,8 +6,11 @@ import org.http4k.routing.routes
 import org.http4k.server.Netty
 import org.http4k.server.asServer
 import org.jetbrains.exposed.sql.Database
+import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
+    val log = LoggerFactory.getLogger("main")
+
     Database.connect("jdbc:h2:mem:example", "org.h2.Driver")
 
     val app = routes(
@@ -16,5 +19,5 @@ fun main(args: Array<String>) {
 
     app.asServer(Netty(8080)).start()
 
-    print("Service ready at port 8080")
+    log.info("Server ready at port 8080")
 }
